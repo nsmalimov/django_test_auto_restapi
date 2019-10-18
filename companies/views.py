2e6aadf0297f360f5ecf4dcb5323a556a4033789
Nurislam Alimov <nalimov@sardinasystems.com>
2019-10-18 10:00:52 +0300

    add put model

--- companies/views.py
+++ companies/views.py
@@ -1,5 +1,8 @@
+import json
 import re
 
+from django.apps import apps
+from django.db.models.fields.related import ForeignKey
 from django.forms.models import model_to_dict
 from django.http import JsonResponse
 
@@ -23,6 +26,7 @@ def parse_params(request, object_meta):
         # но всё равно придётся сплитить
         for sort_param_elem in sort_param_elems:
             if re.match(r'[+\-]\w+', sort_param_elem) is None:
+                # via raise?
                 error_response = JsonResponse({'message':
                                                    'Not correct sort_by param syntax, example: '
                                                    'sort_by=-field_name,+field_name'},
@@ -43,11 +47,35 @@ def parse_params(request, object_meta):
     return filter_params, sort_param, limit, error_response
 
 
+def get_dict_object_by_post_params(request):
+    # проверка на внешние ключи, так как в этом случае надо получить сам объект
+    # из-за автогенерации кода мы не знаем какие именно внешние ключи нам надо будет получить
+    # поэтому тут усложнение
+    app_models = apps.get_app_config('companies').get_models()
+
+    person_request_data = json.loads(request.body)
+
+    for field in Person._meta.get_fields():
+        if isinstance(field, ForeignKey):
+            field_name = str(field).split('.')[-1]
+            for model in app_models:
+                if (field_name == model.__name__.lower()):
+                    try:
+                        person_request_data[field_name] = model.objects.get(id=person_request_data[field_name])
+                    except Exception as e:
+                        raise Exception('Error when try get foreign_key_object: {0}, err: {1}'.format(field_name,
+                                                                                                      str(e)))
+                    break
+
+    return person_request_data
+
+
 def person(request, person_id=None):
     response_dict = {"success": True}
 
     # global try?
     if request.method == 'GET':
+        # todo: validation excess fields
         if not (person_id is None):
             try:
                 person = Person.objects.get(id=person_id)
@@ -75,10 +103,40 @@ def person(request, person_id=None):
 
             response_dict['data'] = persons
     elif request.method == 'POST':
-        print(request.POST)
+        # todo: validation excess fields
+
+        person_dict_from_post = get_dict_object_by_post_params(request)
+
+        person = Person(**person_dict_from_post)
+
+        try:
+            person.save()
+        except Exception as e:
+            return JsonResponse({'message': 'Error when try save person: {0}, err: {1}'.format(person, str(e))},
+                                status=500)
+
+        return JsonResponse(response_dict)
     elif request.method == 'PUT':
+        # todo: validation excess fields
+
         if person_id is None:
             return JsonResponse({'message': 'person_id not set'}, status=400)
+
+        try:
+            person_dict_from_post = get_dict_object_by_post_params(request)
+        except Exception as e:
+            return JsonResponse({'message': 'Error when try get_object_by_post_params request: '
+                                            '{0}, err: {1}'.format(request, str(e))},
+                                status=500)
+
+        try:
+            Person.objects.filter(id=person_id).update(**person_dict_from_post)
+        except Exception as e:
+            return JsonResponse({'message': 'Error when try update person: {0}, err: {1}'.format(person_dict_from_post,
+                                                                                                 str(e))},
+                                status=500)
+
+        return JsonResponse(response_dict)
     elif request.method == 'DELETE':
         if person_id is None:
             return JsonResponse({'message': 'person_id not set'}, status=400)
